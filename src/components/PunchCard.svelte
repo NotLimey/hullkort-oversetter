a1e89e6c60268f1cb111968a57e3892849af3a5b
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-12-16 16:02:17 +0100

    more accurate punch card

--- src/components/PunchCard.svelte
+++ src/components/PunchCard.svelte
@@ -5,7 +5,7 @@
     export let binaries: (0 | 1)[] = [];
 </script>
 
-<div class="bg-orange-300/70 rounded-md px-4 py-2 w-full grid punch-card gap-y-1">
+<div class="bg-orange-300/70 rounded-sm px-4 py-2 w-full grid punch-card gap-y-1 shadow-md">
     {#each binaries as binary}
         {#if binary === 1}
             <div class="bg-white punch rounded-full punched"></div>
